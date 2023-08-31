b06de76105c02dd0ad12213962842762ec5c5b14
김연진 <love_homebody@naver.com>
2023-08-31 17:00:30 +0900

    feat(InputValidator): 잘못 입력한 부분부터 재시작 기능 구현

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -6,46 +6,67 @@ import domain.*;
 import validator.InputValidator;
 
 import java.util.Scanner;
+import java.util.function.Supplier;
 
 public class InputView {
     private static final Scanner scanner = new Scanner(System.in);
 
     public static MainOptions inputMainOption() {
-        System.out.println(OutputMessage.OPTION_SELECT);
-        String input = scanner.nextLine();
-        InputValidator.validateNumber(input);
-        System.out.println();
-        return MainOptions.getMainOptions(Integer.parseInt(input));
+        return attemptedInput(() -> {
+            System.out.println(OutputMessage.OPTION_SELECT);
+            String input = scanner.nextLine();
+            InputValidator.validateNumber(input);
+            System.out.println();
+            return MainOptions.getMainOptions(Integer.parseInt(input));
+        });
     }
 
     public static int inputTableNumber() {
-        System.out.println("## 주문할 테이블을 선택하세요.");
-        String tableNumber = scanner.nextLine();
-        InputValidator.validateNumber(tableNumber);
-        System.out.println();
-        return Integer.parseInt(tableNumber);
+        return attemptedInput(() -> {
+            System.out.println("## 주문할 테이블을 선택하세요.");
+            String tableNumber = scanner.nextLine();
+            InputValidator.validateNumber(tableNumber);
+            System.out.println();
+            return Integer.parseInt(tableNumber);
+        });
     }
 
     public static Menu inputMenu() {
-        System.out.println(OutputMessage.MENU);
-        String input = scanner.nextLine();
-        InputValidator.validateNumber(input);
-        System.out.println();
-        return MenuRepository.findMenuByNumber(Integer.parseInt(input));
+        return attemptedInput(() -> {
+            System.out.println(OutputMessage.MENU);
+            String input = scanner.nextLine();
+            InputValidator.validateNumber(input);
+            System.out.println();
+            return MenuRepository.findMenuByNumber(Integer.parseInt(input));
+        });
     }
 
     public static int inputQuantity() {
-        System.out.println(OutputMessage.QUANTITY);
-        String input = scanner.nextLine();
-        InputValidator.validateNumber(input);
-        System.out.println();
-        return Integer.parseInt(input);
+        return attemptedInput(() -> {
+            System.out.println(OutputMessage.QUANTITY);
+            String input = scanner.nextLine();
+            InputValidator.validateNumber(input);
+            System.out.println();
+            return Integer.parseInt(input);
+        });
+
     }
 
     public static PaymentType inputPayment() {
-        String input = scanner.nextLine();
-        InputValidator.validateNumber(input);
-        System.out.println();
-        return PaymentType.getPaymentType(Integer.parseInt(input));
+        return attemptedInput(() -> {
+            String input = scanner.nextLine();
+            InputValidator.validateNumber(input);
+            System.out.println();
+            return PaymentType.getPaymentType(Integer.parseInt(input));
+        });
+    }
+
+    private static <T> T attemptedInput(Supplier<T> supplier) {
+        try {
+            return supplier.get();
+        } catch (IllegalArgumentException exception) {
+            System.out.println(exception.getMessage());
+            return supplier.get();
+        }
     }
 }
