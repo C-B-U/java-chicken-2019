2ceeffc560de9f101efcc3b4d141f3d6af0e7625
김연진 <love_homebody@naver.com>
2023-08-31 14:30:33 +0900

    feat(Table): 똑같은 메뉴를 100마리 이상 시킬 경우 예외 발생

--- src/main/java/constant/ExceptionMessage.java
+++ src/main/java/constant/ExceptionMessage.java
@@ -4,7 +4,8 @@ public enum ExceptionMessage {
 
     INCORRECT_MAIN_OPTION("존재하지 않는 기능입니다."),
     NOT_EXIST_MENU("존재하지 않는 메뉴입니다."),
-    NOT_EXIST_TABLE("존재하지 않는 테이블입니다.");
+    NOT_EXIST_TABLE("존재하지 않는 테이블입니다."),
+    ORDER_QUANTITY_EXCEEDED("주문 수량을 초과하였습니다.");
 
     private static final String PREFIX = "[ERROR] ";
     private final String message;
--- src/main/java/domain/Table.java
+++ src/main/java/domain/Table.java
@@ -1,11 +1,14 @@
 package domain;
 
+import constant.ExceptionMessage;
+
 import java.util.HashMap;
 import java.util.Map;
 
 public class Table {
 
     private static final int DEFAULT = 0;
+    private static final int MAX_ORDER_QUANTITY = 100;
 
     private final int number;
     private final Map<Menu, Integer> order;
@@ -22,8 +25,15 @@ public class Table {
     }
 
     public void addMenu(Menu menu, int quantity) {
-        int existingQuantity = order.get(menu);
-        order.put(menu, existingQuantity += quantity);
+        int newQuantity = order.get(menu) + quantity;
+        validateQuantity(newQuantity);
+        order.put(menu, newQuantity);
+    }
+
+    private void validateQuantity(int quantity) {
+        if (quantity >= MAX_ORDER_QUANTITY) {
+            throw new IllegalArgumentException(ExceptionMessage.ORDER_QUANTITY_EXCEEDED.toString());
+        }
     }
 
     @Override
