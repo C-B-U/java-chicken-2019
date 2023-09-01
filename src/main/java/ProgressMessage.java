a991bda8443e64024c5e58718b9419a740cc9f44
GaBaljaintheroom <junsu1222@naver.com>
2023-09-01 16:39:22 +0900

    feat: ProgressMessage 테이블 메시지 추가

--- src/main/java/ProgressMessage.java
+++ src/main/java/ProgressMessage.java
@@ -1,6 +1,8 @@
 public enum ProgressMessage {
     MAIN_MESSAGE("## 메인화면"),
-    SELECT_NUMBER("## 원하는 기능을 선택하세요.");
+    SELECT_NUMBER("## 원하는 기능을 선택하세요."),
+    TABLE_LIST("## 테이블 목록"),
+    SELECT_TABLE("## 테이블을 선택하세요.");
 
     private final String message;
 
