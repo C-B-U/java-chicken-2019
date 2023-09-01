1845944599ac2875fa6dc5375a8e72b605869ba6
김연진 <love_homebody@naver.com>
2023-09-01 17:31:14 +0900

    test(ChickenService): 서비스 테스트

--- src/main/java/constant/ExceptionMessage.java
+++ src/main/java/constant/ExceptionMessage.java
@@ -6,7 +6,8 @@ public enum ExceptionMessage {
     NOT_EXIST_MENU("존재하지 않는 메뉴입니다."),
     NOT_EXIST_TABLE("존재하지 않는 테이블입니다."),
     ORDER_QUANTITY_EXCEEDED("주문 수량을 초과하였습니다."),
-    NOT_NUMBER("숫자만 입력 가능합니다.");
+    NOT_NUMBER("숫자만 입력 가능합니다."),
+    NOT_EXIST_PAYMENT_TYPE("존재하지 않는 테이블입니다."),;
 
     private static final String PREFIX = "[ERROR] ";
     private final String message;
