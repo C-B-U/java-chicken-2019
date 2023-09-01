27fb63f4e17289fd8c896b83525c2714d712d2de
GaBaljaintheroom <junsu1222@naver.com>
2023-09-01 16:40:25 +0900

    feat: Menu 생성

---
+++ src/main/java/Menu.java
@@ -0,0 +1,41 @@
+
+import java.util.Arrays;
+import java.util.List;
+
+public enum Menu {
+
+    MENU1("치킨", 1, "후라이드", 16000),
+    MENU2("치킨", 2, "양념치킨", 16000),
+    MENU3("치킨", 3, "반반치킨", 16000),
+    MENU4("치킨", 4, "통구이", 16000),
+    MENU5("치킨", 5, "간장치킨", 17000),
+    MENU6("치킨", 6, "순살치킨", 17000),
+    MENU21("음료", 21, "콜라", 1000),
+    MENU22("음료", 22, "사이다", 1000);
+
+    private final String type;
+    private final int number;
+    private final String name;
+    private final int price;
+
+    Menu(String type, int number, String name, int price) {
+        this.type = type;
+        this.number = number;
+        this.name = name;
+        this.price = price;
+    }
+
+    public static void addMenu (List<Menu> menus){
+        menus.addAll(Arrays.asList(Menu.values()));
+    }
+
+    public static String showMenu(){
+        StringBuilder menuString = new StringBuilder();
+        for (Menu menu : values()) {
+            menuString.append(String.format("[%s] %d - %s : %d원\n", menu.type, menu.number, menu.name, menu.price));
+        }
+        return menuString.toString();
+    }
+
+}
+
