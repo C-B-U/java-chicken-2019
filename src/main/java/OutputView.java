f589f753b98bd5a032a0e631af79e710754041a9
GaBaljaintheroom <junsu1222@naver.com>
2023-09-01 16:39:53 +0900

    feat: OutputView 테이블 메시지 추가

--- src/main/java/OutputView.java
+++ src/main/java/OutputView.java
@@ -1,11 +1,28 @@
 public class OutputView {
 
-    public void printChoicesProgram(){
+    public void printMainProgram(){
         System.out.println(ProgressMessage.MAIN_MESSAGE);
+
+    }
+
+    public void printSelectMainNumber(){
         System.out.println(ProgressMessage.SELECT_NUMBER);
     }
 
     public void printErrorMessage(String message){
         System.out.println(message);
+    }
+
+    public void printTableList(TableList tableList){
+        System.out.println(ProgressMessage.TABLE_LIST);
+        System.out.println(tableList);
+    }
+
+    public void printSelectTableNumber(){
+        System.out.println(ProgressMessage.SELECT_TABLE);
+    }
+
+    public void printMenuList(){
+        System.out.println(Menu.showMenu());
     }
 }
