ea8f345b51905f9e695876ec49529e23f1520c6a
GaBaljaintheroom <junsu1222@naver.com>
2023-09-01 16:37:40 +0900

    feat: ErrorMessage INPUT_TABLE_NUMBER_ERROR추가

--- src/main/java/ErrorMessage.java
+++ src/main/java/ErrorMessage.java
@@ -1,5 +1,6 @@
 public enum ErrorMessage {
-    INPUT_MAIN_NUMBER_ERROR("기능 번호는 %s 부터 %s 입니다.");
+    INPUT_MAIN_NUMBER_ERROR("기능 번호는 %s 부터 %s 입니다."),
+    INPUT_TABLE_NUMBER_ERROR("옳지 않는 테이블 번호입니다.");
 
     private static final String ERROR = "[ERROR]";
     private final String message;
