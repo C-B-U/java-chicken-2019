7a68a087027fa5c1c020060c5e7ca721d8fa6024
김연진 <love_homebody@naver.com>
2023-09-01 14:08:11 +0900

    refactor(ChickenController): 메서드 분리

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -2,9 +2,8 @@ import controller.ChickenController;
 
 public class Application {
 
-    // TODO 구현 진행
     public static void main(String[] args) {
         ChickenController controller = new ChickenController();
-        controller.start();
+        controller.run();
     }
 }
--- src/main/java/controller/ChickenController.java
+++ src/main/java/controller/ChickenController.java
@@ -9,40 +9,47 @@ public class ChickenController {
 
     private final ChickenService service = new ChickenService();
 
-    public void start() {
+    public void run() {
         try {
-            while (true) {
-                OutputView.printMainOption();
-                MainOptions option = InputView.inputMainOption();
-                if (option.isTermination()) {
-                    break;
-                }
-                orderMenu(option);
-                payment(option);
-            }
+            startSelling();
         } catch (IllegalArgumentException exception) {
             OutputView.printExceptionMessage(exception.getMessage());
         }
     }
 
+    private void startSelling() {
+        while (true) {
+            OutputView.printMainOption();
+            MainOptions option = InputView.inputMainOption();
+            if (option.isTermination()) {
+                break;
+            }
+            orderMenu(option);
+            payment(option);
+        }
+    }
+
     private void orderMenu(MainOptions options) {
         if (options.isOrder()) {
-            OutputView.printTables(TableRepository.tables());
-            int table = InputView.inputTableNumber();
+            int tableNumber = findTable();
             OutputView.printMenus(MenuRepository.menus());
             Menu menu = InputView.inputMenu();
             int quantity = InputView.inputQuantity();
-            service.order(table, menu, quantity);
+            service.order(tableNumber, menu, quantity);
         }
     }
 
     private void payment(MainOptions options) {
         if (options.isPayment()) {
-            OutputView.printTables(TableRepository.tables());
-            Table table = service.findByTableNumber(InputView.inputTableNumber());
+            Table table = service.findByTableNumber(findTable());
             OutputView.printOrderHistory(table);
             OutputView.printPayment(table);
             OutputView.printTotalPrice(service.payment(table, InputView.inputPayment()));
         }
+    }
+
+    private int findTable() {
+        OutputView.printTables(TableRepository.tables());
+        return InputView.inputTableNumber();
     }
 }
