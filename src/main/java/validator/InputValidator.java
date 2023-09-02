1cf25503b5e3c422c215df5c758c6f7502a95ecb
Jiyoon Kim <rlawldbs0911@naver.com>
2023-09-03 07:13:52 +0900

    refactor(InputValidator): 유효성 검사 정규표현식 수정

--- src/main/java/validator/InputValidator.java
+++ src/main/java/validator/InputValidator.java
@@ -5,9 +5,11 @@ import domain.ErrorMessage;
 import java.util.regex.Pattern;
 
 public class InputValidator {
-    private static final String REGIX = "^[\\d]*$";
+    private static final String REGEX = "^[0-9]+$";
+
     public void validateIsDigit(String number) {
-        if (!Pattern.matches(number, REGIX)) {
+        if (Pattern.matches(number, REGEX)) {
+            System.out.println(Pattern.matches(number, REGEX));
             throw new IllegalArgumentException(ErrorMessage.WRONG_FEATURE_NUMBER.toString());
         }
     }
