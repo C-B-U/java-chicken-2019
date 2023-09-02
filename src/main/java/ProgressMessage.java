f8bd96dddb78fb8acc06b64ef3ed3e2eab9c6542
GaBaljaintheroom <junsu1222@naver.com>
2023-09-02 11:03:45 +0900

    feat: ProgressMessage 출력문 추가

--- src/main/java/ProgressMessage.java
+++ src/main/java/ProgressMessage.java
@@ -2,7 +2,9 @@ public enum ProgressMessage {
     MAIN_MESSAGE("## 메인화면"),
     SELECT_NUMBER("## 원하는 기능을 선택하세요."),
     TABLE_LIST("## 테이블 목록"),
-    SELECT_TABLE("## 테이블을 선택하세요.");
+    SELECT_TABLE("## 테이블을 선택하세요."),
+    SELECT_MENU("## 등록할 메뉴를 선택하세요."),
+    INPUT_MENU_QUANTITY("## 메뉴의 수량을 입력하세요.");
 
     private final String message;
 
