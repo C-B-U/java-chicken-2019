95de9dc9a71ec7971a38cd47011d3332c6a086ad
GaBaljaintheroom <junsu1222@naver.com>
2023-09-02 15:40:34 +0900

    feat: ErrorMessage 추가

--- src/main/java/ErrorMessage.java
+++ src/main/java/ErrorMessage.java
@@ -2,7 +2,8 @@ public enum ErrorMessage {
     INPUT_MAIN_NUMBER_ERROR("기능 번호는 %s 부터 %s 입니다."),
     INPUT_TABLE_NUMBER_ERROR("옳지 않는 테이블 번호입니다."),
     INPUT_MENU_NUMBER_ERROR("옳지 않는 메뉴 번호입니다."),
-    INPUT_MENU_QUANTITY_ERROR("한 테이블에서 주문할 수 있는 한 메뉴의 최대 수량은 %s 개입니다.");
+    INPUT_MENU_QUANTITY_ERROR("한 테이블에서 주문할 수 있는 한 메뉴의 최대 수량은 %s 개입니다."),
+    INPUT_PAYMENT_NUMBER_ERROR("신용 카드는 %s번, 현금은 %s번 입니다.");
 
     private static final String ERROR = "[ERROR]";
     private final String message;
