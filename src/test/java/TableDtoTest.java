e632f60d03fd900941d5ac25a4ac31a9698193db
GaBaljaintheroom <junsu1222@naver.com>
2023-09-06 11:47:55 +0900

    test: 테이블 번호와 일치하는 테이블을 반환

--- src/test/java/TableDtoTest.java
+++ src/test/java/TableDtoTest.java
@@ -7,10 +7,11 @@ import static org.assertj.core.api.Assertions.*;
 
 class TableDtoTest {
 
+    TableList tableList = new TableList();
+
     @Test
     @DisplayName("초기화된 테이블들이 저장되는지 확인한다.")
     void saveInitTables(){
-        TableList tableList = new TableList();
         List<Table> tables = tableList.getTables();
 
         List<Integer> tableNumbersToCheck = List.of(1,2,3,5,6,8);
@@ -18,5 +19,12 @@ class TableDtoTest {
             Table table = tables.get(i);
             assertThat(table.getNumber()).isEqualTo(tableNumbersToCheck.get(i));
         }
+    }
+
+    @Test
+    @DisplayName("테이블 번호와 일치하는 테이블을 반환한다.")
+    void getTableByTableId(){
+        Table table = tableList.getTable(1);
+        assertThat(table.getNumber()).isEqualTo(1);
     }
 }
